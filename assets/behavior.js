3122232f84dd92b566627d6871b9dc936f08f0e5
blostic <piotr.skibiak@gmail.com>
2014-06-07 14:11:05 +0200

    Generated documentation for 7d1681d2f2283a8bdd4458f5deffba75ad836964

--- assets/behavior.js
+++ assets/behavior.js
@@ -150,8 +150,8 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "type": "heading",
               "data": {
                 "level": 2,
-                "title": "Specify instances",
-                "slug": "specify-instances"
+                "title": "Ping ",
+                "slug": "ping-"
               },
               "depth": 2
             }
